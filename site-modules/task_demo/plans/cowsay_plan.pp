bb75b9cee5568cbc74979b2c4c733ba13fa2cbe6
Garrett Rowell <garrett.rowell@puppet.com>
2020-03-05 01:56:42 -0500

    adsf

--- site-modules/task_demo/plans/cowsay_plan.pp
+++ site-modules/task_demo/plans/cowsay_plan.pp
@@ -1,7 +1,16 @@
-plan task_demo::cowsay_plan(){
+plan task_demo::cowsay_plan(
+  Boolean $cleanup = false,
+){
   $nodes = get_target('pe-201922-agent.puppetdebug.vlan')
-  run_task('package', $nodes, action => install, name => 'epel-release')
-  run_task('package', $nodes, action => install, name => 'cowsay')
-  run_task('package', $nodes, action => install, name => 'fortune-mod')
+
+  if $cleanup {
+    run_task('package', $nodes, action => uninstall, name => 'epel-release')
+    run_task('package', $nodes, action => uninstall, name => 'cowsay')
+    run_task('package', $nodes, action => uninstall, name => 'fortune-mod')
+  } else {
+    run_task('package', $nodes, action => install, name => 'epel-release')
+    run_task('package', $nodes, action => install, name => 'cowsay')
+    run_task('package', $nodes, action => install, name => 'fortune-mod')
+  }
 
 }

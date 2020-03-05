22df6b26d3d5f0510be4e4d285f1d41e93ac5749
Garrett Rowell <garrett.rowell@puppet.com>
2020-03-05 01:51:42 -0500

    asdfas

--- site-modules/task_demo/plans/cowsay_plan.pp
+++ site-modules/task_demo/plans/cowsay_plan.pp
@@ -1,4 +1,7 @@
 plan task_demo::cowsay_plan(){
   $nodes = get_target('pe-201922-agent.puppetdebug.vlan')
-  run_command('whoami', $nodes)
+  run_task('package', $nodes, action => install, name => 'epel-release')
+  run_task('package', $nodes, action => install, name => 'cowsay')
+  run_task('package', $nodes, action => install, name => 'fortune-mod')
+
 }
